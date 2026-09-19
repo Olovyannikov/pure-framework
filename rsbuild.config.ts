65757d5ac75e5ca2d331fae22bd43bdf466ac2d2
Ilya Olovyannikov <excaliburgyro@gmail.com>
2026-09-19 10:25:42 +0300

    chore: add cz

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,4 +1,12 @@
 import { defineConfig } from '@rsbuild/core';
 
 // Docs: https://rsbuild.rs/config/
-export default defineConfig({});
+export default defineConfig({
+  output: {
+    cssModules: {
+      auto: (resource) => {
+        return resource.includes('.module.') || resource.includes('shared/');
+      },
+    },
+  },
+});
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import './index.css';
+import s from './index.module.css';
 
 const rootEl = document.querySelector('#root');
 if (rootEl) {
   rootEl.innerHTML = `
-  <div class="content">
+  <div class=${s.content}>
     <h1>Vanilla Rsbuild</h1>
     <p>Start building amazing things with Rsbuild.</p>
   </div>
